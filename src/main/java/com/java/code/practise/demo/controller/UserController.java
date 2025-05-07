84f252334742fd7320f9a9d1a262624616b1dce7
lmlilm <lmlilm@cn.ibm.com>
2025-05-07 22:40:50 +0800

    second commit

--- src/main/java/com/java/code/practise/demo/controller/UserController.java
+++ src/main/java/com/java/code/practise/demo/controller/UserController.java
@@ -3,6 +3,7 @@ package com.java.code.practise.demo.controller;
 import com.java.code.practise.demo.dto.CreateUserResponse;
 import com.java.code.practise.demo.entity.User;
 import com.java.code.practise.demo.service.UserService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,11 +19,11 @@ public class UserController {
     UserService userService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> findUserById(@PathVariable Long id) {
-        User user = userService.findById(id);
-        if (user != null) {
+    public ResponseEntity<User> findUserById(@PathVariable("id") Long id) {
+        try {
+            User user = userService.findById(id);
             return ResponseEntity.ok(user);
-        } else {
+        } catch (EntityNotFoundException e) {
             return ResponseEntity.notFound().build();
         }
     }
